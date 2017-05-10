547513633984f13ac198df4094c6f5090f8d821d
Gilia Gongora <giliagongora.g@gmail.com>
2017-05-09 22:41:20 -0300

    finalizado

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/*
+//desarrollo ejercicio ==> forma imperativa
+//input = l1=[1,2,3],l2=[2,3,4] - out= [2,3]
+
+function intersection(l1,l2){
+	var l3=[]
+	for(var i=0 ; i<l1.lenght; i++){
+		for(var j=0; j<l2.lenght;j++){
+			if(l1[i]==l2[j]){
+				l3.push(l1[i]);
+			}
+		}
+	return l3;
+}
+}
+
+// ejercicio desarrollado de forma declarativa
+function intersectionSet2(l1,l2){
+	return l1.filter( function(n){
+		return l2.indexOf(n) !== -1
+	});
+};
+*/
+
+var productos = [
+{
+ nombre: 'pijama',
+ tipo: 'ropa'
+},
+ {
+ nombre: 'nevera',
+ tipo: 'electrodomestico'	
+},
+ {
+  nombre: 'asus',
+  tipo: 'computador'
+ },
+  {
+  nombre: 'macbook',
+  tipo: 'computador'
+ },
+  {
+   nombre: 'pijama',
+   tipo: '´ropa'
+ },
+  {
+  nombre: 'toshiba', tipo: 'computador'
+ }
+];
+
+/*
+var computadores = [];
+for(var i=0; i<productos.length; i++){
+  if(productos[i].tipo === 'computador'){
+  	computadores.push(productos[i]);
+  }
+}
+*/
+
+var computadores = productos.filter(function(producto){
+	return producto.tipo === 'computador';
+});
